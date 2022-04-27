7fc8c3ea60b2502788bf4936c43ad744c312755a
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-04-27 20:29:25 +0300

    user list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-import Buttons from './Buttons'
+import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div className="App">
-      <div className='btns'>
-        <Buttons bg="blue" size="base" text="My Button Blue"/>
-        <Buttons bg="gray" size="base" text="My Button Gray"/>
-        <Buttons bg="green" size="base" text="My Button Green"/>
-        <Buttons bg="orange" size="base" text="My Button Orange"/>
-        <Buttons bg="red" size="base" text="My Button Red"/>
-        <Buttons bg="sky" size="base" text="My Button Sky"/>
-        <Buttons bg="white" size="base" text="My Button White"/>
-        <Buttons bg="dark" size="base" text="My Button Dark"/>
-        <Buttons bg="transparent" size="base" text="My Button Transparent"/>
-      </div>
-      <div className='btns'>
-        <Buttons bg="blue" size="big" text="My Button Big"/>
-        <Buttons bg="blue" size="small" text="My Button Small"/>
-      </div>
-      <div className='btns'>
-        <Buttons outline="outline" border="red" size="base" text="My Button Outline Red"/>
-        <Buttons outline="outline" border="green" size="base" text="My Button Outline Green"/>
-      </div>
+    <div className='main'>
+      <h1 className='title'>Список користувачів</h1>
+      <UsersList/>
     </div>
   );
 }
--- src/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import './Button.css'
-
-function Buttons({bg, size, outline, border, text}) {
-    return (
-        <button className={`btn btn-${bg || ''} btn-${size || ''} btn-${outline || ''} btn-border-${border || ''}`}>{text}</button>
-    )
-}
-
-export default Buttons
